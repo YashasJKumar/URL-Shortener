4bc0373bcb2422c200bda829b6feb7d8b78bde56
Yashas J Kumar <96472550+YashasJKumar@users.noreply.github.com>
2025-02-13 22:37:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cookieParser = require('cookie-parser')
+const path = require('path');
+
+const URLRouter = require('./Routes/routes')
+const StaticRouter = require("./Routes/static_router")
+const UserRouter = require("./Routes/auth")
+
+const { connectMongoDb } = require("./connection")
+const {restrictToLoggedInUser, checkAuth} = require("./Middlewares/auth")
+
+const app = express();
+const port = 8001;
+
+app.set("view engine", "ejs")
+app.set("views", path.resolve("./views"))
+
+connectMongoDb("mongodb://localhost:27017/url-shortener")
+    .then(r => console.log("MongoDB Connected")).catch(err => console.error(err));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+// false when working with simple form data.
+// true when handling nested objects or complex data.
+
+app.use("/url", restrictToLoggedInUser, URLRouter)
+// Inline middleware only for /url
+app.use("/", checkAuth, StaticRouter)
+app.use('/user', UserRouter)
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
