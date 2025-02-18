58f24d33c514339bc9330ebe878339d2adf33a72
Yashas J Kumar <96472550+YashasJKumar@users.noreply.github.com>
2025-02-18 11:10:39 +0530

    Update static_router.js

--- Routes/static_router.js
+++ Routes/static_router.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const URL = require("../Models/schemas")
+const {allowAccesTo} = require("../Middlewares/auth");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-    if(!req.user)
-        return res.redirect('/login')
+router.get("/",allowAccesTo(['NORMAL', 'ADMIN']), async (req, res) => {
     const urls = await URL.find({ createdBy: req.user._id})
     return res.render('home', {urls: urls})
 })
@@ -15,6 +14,12 @@ router.get('/sign-up', (req, res) => {
 
 router.get('/login', (req, res) => {
     return res.render('login')
+})
+
+router.get("/admin/dashboard", allowAccesTo(['ADMIN']), async (req, res) => {
+    // ADMIN -> Can see all URL's
+    const allUrls = await URL.find()
+    return res.render('home', {urls: allUrls})
 })
 
 module.exports = router;
