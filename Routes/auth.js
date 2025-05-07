72a15e1700a75de3d495e57381d026e54fb4280a
Yashas J Kumar <96472550+YashasJKumar@users.noreply.github.com>
2025-05-07 21:16:39 +0530

    Update auth.js

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const { handleGenerateShortUrl,
-    handleRedirectShortUrl } = require('../Controllers/handlers')
-const router = express.Router();
+const express = require('express')
+const { handleUserSignUp, handleUserLogin, handleForgotPassword, handleResetPasswordCheck, handleResetPassword} = require('../Controllers/user')
+const router = express.Router()
 
+router.post('/', handleUserSignUp)
+router.post('/login', handleUserLogin)
+router.get('/logout', (req, res) => {
+    res.clearCookie('token').redirect('/')
+})
+router.post("/forgot-password", handleForgotPassword)
 
-router.post('/', handleGenerateShortUrl);
+router.get("/reset-password/:id/:token", handleResetPasswordCheck);
 
-router.get('/:shortUrl', handleRedirectShortUrl)
-
-module.exports = router;
+router.post("/reset-password/:id/:token", handleResetPassword);
+module.exports = router
